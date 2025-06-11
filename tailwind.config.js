e760e9b9d6cb0dce974519a724f167a0592317f0
nasir <nasiradan916@gmail.com>
2025-06-11 13:20:35 +0300

    Add Tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+/** @type {import('tailwindcss').Config} */ 
+module.exports = { 
+  content: [ 
+    "./src//*.{js,jsx,ts,tsx}", 
+  ], 
+  theme: { 
+    extend: {}, 
+  }, 
+  plugins: [], 
 }
-
